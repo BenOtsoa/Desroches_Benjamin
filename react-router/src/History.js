b4fcdb174bcf7b4252ade11b462bd70b5b3a7517
BJDsrchs <benjamin.desroches@gmail.com>
2018-05-28 19:47:10 +0200

    react-router

---
+++ react-router/src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class History extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Notre Histoire</div>;
+  }
+}
+
+export default History;
