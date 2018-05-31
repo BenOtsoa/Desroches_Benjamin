d1e28dda687b45d3c6827aedad77c911607eed6c
BJDsrchs <benjamin.desroches@gmail.com>
2018-05-31 19:41:38 +0200

    step by step

--- Back/app.js
+++ Back/app.js
@@ -4,9 +4,11 @@ const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const db = require(`./models/index.js`);
 
 const index = require("./routes/index");
 const users = require("./routes/users");
+const about = require("./routes/about");
 
 const app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 app.use("/users", users);
+app.use("/about", about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Back/routes/index.js
+++ Back/routes/index.js
@@ -6,8 +6,4 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/", function(req, res, next) {
-  router.get("/about", (req, res) => controller.about(req, res));
-});
-
 module.exports = router;
