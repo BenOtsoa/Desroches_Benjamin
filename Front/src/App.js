e68b7cf730544e64000c7137d167381e0e060aed
BJDsrchs <benjamin.desroches@gmail.com>
2018-06-01 09:53:59 +0200

    fusion quêtes

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
+import { NavLink, Route } from "react-router-dom";
 import MyTimer from "./MyTimer";
 import SignUp from "./SignUp";
+import Home from "./Home";
+import History from "./History";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date()
-    };
-  }
-
   render() {
     return (
       <div className="App">
         <MyTimer />
         <SignUp />
+        <ul>
+          <NavLink to="/home" className="style-link" activeClassName="selected">
+            Home
+          </NavLink>
+          <NavLink
+            to="/notre-histoire"
+            className="style-link"
+            activeClassName="selected"
+          >
+            History
+          </NavLink>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/notre-histoire" component={History} />
       </div>
     );
   }
